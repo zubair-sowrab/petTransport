661fc2427cf54664e47e07236f09e49655a7ed15
MD. ZAKARIA KHAN <107675204+Zakaria177@users.noreply.github.com>
2024-05-17 23:43:57 +0600

    Add files via upload

---
+++ assets/data/cars.js
@@ -0,0 +1,24 @@
+export default [
+    {
+      id: '0',
+      type: 'Bike',
+      latitude: 23.792979,
+      longitude: 90.354626,
+      heading: 47,
+    },
+    {
+      id: '1',
+      type: 'Car',
+      latitude: 23.793450,
+      longitude: 90.352224,
+      heading: 190,
+    },
+    {
+      id: '2',
+      type: 'Large',
+      latitude: 23.790623,
+      longitude: 90.355999,
+      heading: 180,
+    },
+   
+  ];
